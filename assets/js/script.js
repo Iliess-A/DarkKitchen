7c5aa9ef49b536ebb39870f056e5409d8b173fc7
iliess <iliessabdelmadjid@gmail.com>
2024-04-23 15:02:12 +0200

    ajout du css pour les cartes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const table=[
-    ['assets/img/hamburger.jpg','Hamburger','buff,salade,ognion,cheddar,ketchup,mutard,cornichon','14,99€','5/5','carnivore'],
+    ['assets/img/hamburger.jpg','Hamburger','buff,salade,ognion,cheddar,ketchup,mutard,cornichon',14.99,'5/5','carnivore'],
     ['image','nom','ingrediant','price','rating(sur 5)','filter'],
 ]
+
+let tot=[];
 
 for(i=0;i<table.length;i++){
 
@@ -13,7 +15,7 @@ for(i=0;i<table.length;i++){
     const image =document.createElement('img');
     image.setAttribute('src',table[i][0]);
     image.setAttribute('alt','image de burger')
-    image.style.width='100px';
+    image.style.width='150px';
     div.appendChild(image);
 
     let titre=document.createElement('h2');
@@ -22,18 +24,27 @@ for(i=0;i<table.length;i++){
 
     let description= document.createElement('p');
     description.textContent=table[i][2];
+    description.className='description';
     div.appendChild(description);
 
     let price= document.createElement('p');
     price.textContent=table[i][3];
+    price.className='price';
     div.appendChild(price);
 
     let note= document.createElement('p');
     note.textContent=table[i][4];
+    note.className='note';
     div.appendChild(note);
 
     let categorie= document.createElement('button')
     categorie.textContent=table[i][5];
+    categorie.className='categorie';
     div.appendChild(categorie);
+
+    let add= document.createElement('button');
+    add.textContent='add';
+    add.className='add';
+    div.appendChild(add);
 }
 
